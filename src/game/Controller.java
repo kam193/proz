867e85ac31a263a1efa3fb8d55b529b5ae51412b
Kamil Mankowski <kam193@wp.pl>
2018-05-21 12:16:53 +0200

    Dodanie wrogo

--- src/game/Controller.java
+++ src/game/Controller.java
@@ -4,18 +4,31 @@ import javafx.fxml.FXML;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
+import model.Enemy;
 import model.Player;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Controller {
 
     public Pane paneGame;
     private Player player;
+    private List<Enemy> enemies = new ArrayList<>();
 
     @FXML
     public void initialize(){
         player = new Player(300, 500);
 
         paneGame.getChildren().add(player.getView());
+
+        enemies.add(new Enemy(250, Enemy.EnemyType.ICECREAM));
+        enemies.add(new Enemy(300, Enemy.EnemyType.CAKE));
+        enemies.add(new Enemy(350, Enemy.EnemyType.ICECREAM));
+
+        paneGame.getChildren().addAll(enemies.stream().map(e -> e.getView()).collect(Collectors.toList()));
         paneGame.getParent().setOnKeyPressed(this::pressedKey);
 
     }
--- src/model/Enemy.java
+++ src/model/Enemy.java
@@ -3,8 +3,30 @@ package model;
 import javafx.scene.shape.Circle;
 
 public class Enemy extends GameElement{
-    public Enemy(double startX){
-        super(new Circle(20));
 
+    private EnemyType type;
+
+    public Enemy(double startX, EnemyType typeEnemy){
+        super(new Circle(20));
+        type = typeEnemy;
+        getView().getStyleClass().add(type.styleClassName);
+        getView().setCenterX(startX);
+        getView().setCenterY(25);
+
+    }
+
+    public EnemyType getType() {
+        return type;
+    }
+
+    public enum EnemyType{
+        ICECREAM("icecream"),
+        CAKE("cake");
+
+        String styleClassName;
+
+        private EnemyType(String className){
+            styleClassName = className;
+        }
     }
 }
