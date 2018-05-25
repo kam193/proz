2b3dfbc46fb4622b2f59b836852c32af7545fe28
Kamil Mankowski <kam193@wp.pl>
2018-05-25 13:26:58 +0200

    Komentarze do GameStats

--- src/model/game/GameStats.java
+++ src/model/game/GameStats.java
@@ -11,12 +11,19 @@ public class GameStats {
     private ObservableList<Integer> killedEnemies = FXCollections.observableArrayList(new ArrayList<>());
     private SimpleIntegerProperty pointsProperty = new SimpleIntegerProperty();
 
+    /**
+     * Create new stat. Prepare space to count killed
+     */
     public GameStats() {
         for (int i = 0; i < Enemy.EnemyType.values().length; i++) {
             killedEnemies.add(0);
         }
     }
 
+    /**
+     * Increase count of killed
+     * @param type Type of killed enemy
+     */
     public void addKilledEnemy(Enemy.EnemyType type) {
         pointsProperty.set(pointsProperty.getValue() + type.health);
         killedEnemies.set(type.ordinal(), killedEnemies.get(type.ordinal()) + 1);
@@ -34,6 +41,9 @@ public class GameStats {
         return killedEnemies;
     }
 
+    /**
+     * Clear statistics
+     */
     public void clear() {
         pointsProperty.set(0);
         for (int i = 0; i < Enemy.EnemyType.values().length; i++) {
